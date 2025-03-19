1225a507cb8b975baa18cfc4dcccb7206588c531
Taina SSantos Silva <tainassilva25111996@gmail.com>
2025-03-19 11:00:05 -0300

    docs: Adicionando um comentário para lembrar de implementar esse método.

--- src/main/java/br/com/taina/exception/NenhumaAlteracaoException.java
+++ src/main/java/br/com/taina/exception/NenhumaAlteracaoException.java
@@ -1,18 +1,17 @@
 package br.com.taina.exception;
 
+import java.io.Serial;
+
 /**
  * Exceção personalizada lançada quando não há alterações para serem atualizadas.
  */
+
+// Ver uma forma de implementar isso ...
 public class NenhumaAlteracaoException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    /**
-    * Recebe uma mensagem padrão de erro será utilizada:
-    * "Não há alterações para atualizar."
-    * 
-    * @param message A mensagem de erro a ser associada a exceção.
-    */
     public NenhumaAlteracaoException(String message) {
         super("Não há alterações para atualizar.");
     }
