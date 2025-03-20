654361adf93b0daab01227525c47d0be43f5f3f6
Taina SSantos Silva <tainassilva25111996@gmail.com>
2025-03-20 10:21:16 -0300

    chore: Mudanças da estrutura de pastas para melhor organização.

---
+++ src/main/java/br/com/taina/dto/login/LoginRequest.java
@@ -0,0 +1,4 @@
+package br.com.taina.dto.login;
+
+public record LoginRequest(String email, String senha) {
+}
---
+++ src/main/java/br/com/taina/dto/login/LoginResponse.java
@@ -0,0 +1,4 @@
+package br.com.taina.dto.login;
+
+public record LoginResponse (String accessToken, long expiresIn){
+}
