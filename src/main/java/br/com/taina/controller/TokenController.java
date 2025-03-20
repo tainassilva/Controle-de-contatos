a056138aafc604229cfbe701fb0110e8e4cd7404
Taina SSantos Silva <tainassilva25111996@gmail.com>
2025-03-20 11:52:05 -0300

    fix: Arrumando a lógica para caso o email não exista no banco de dados e a senha estiver inválida. Na lógica antiga a função isLoginCorrect não estava negada

--- src/main/java/br/com/taina/controller/TokenController.java
+++ src/main/java/br/com/taina/controller/TokenController.java
@@ -9,15 +9,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 
 @RestController
-@RequestMapping("/login")
 public class TokenController {
 
     private final JwtEncoder jwtEncoder;
@@ -33,14 +32,17 @@ public class TokenController {
     }
 
 
-    @PostMapping
+    @PostMapping("/login")
+    @Transactional
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
 
+        // lógica de banco de dados que vai para dentro do service
         var user = pessoaRepository.findByEmail(loginRequest.email());
 
-        if(user.isEmpty() || user.get().isLoginCorrect(loginRequest, passwordEncoder)){
+        if(user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)){
             throw new BadCredentialsException("Usuário ou senha inválida!");
         }
+
         var now = Instant.now();
         var expiresIn = 1440L;
 
