05d50636dcea58db9035bf153bf2256f7010ef9c
Taina SSantos Silva <tainassilva25111996@gmail.com>
2025-03-20 10:22:40 -0300

    Criando o repository para role fazer as operações de banco de dados

---
+++ src/main/java/br/com/taina/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package br.com.taina.repository;
+
+import br.com.taina.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByNome(String nome);
+}
